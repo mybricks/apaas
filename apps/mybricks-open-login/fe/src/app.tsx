8db310826dd1048d601d4369480a7165c949b1e5
liubiao03 <cocolbell@163.com>
2024-06-13 10:35:56 +0800

    feat: login page update

--- apps/mybricks-open-login/fe/src/app.tsx
+++ apps/mybricks-open-login/fe/src/app.tsx
@@ -232,102 +232,102 @@ export default function App() {
         <Logo />
         <p className={css.headTitle}>MyBricks</p>
       </div>
-      <div className={css.body}>
-        <div className={css.content}>
-          {(action.type === 'signin' || action.type === 'signup') && (
-            <form onSubmit={onSubmit} className={css.form} method="post">
-              <div className={css.title}>
-                {action.type === 'signin' ? '使用 MyBricks' : '注册新用户'}
-              </div>
+      <div className={css.view}>
+        <div className={css.body}>
+          <div className={css.content}>
+            {(action.type === 'signin' || action.type === 'signup') && (
+              <form onSubmit={onSubmit} className={css.form} method="post">
+                <div className={css.title}>
+                  {action.type === 'signin' ? '使用 MyBricks' : '注册新用户'}
+                </div>
 
-              <input
-                type="text"
-                className={errInfo.type === 'email' ? css.err : ''}
-                value={formData.email}
-                onChange={(e) => {
-                  onChangeFormData(e, 'email')
-                }}
-                placeholder={'邮箱地址'}
-              />
-              <input
-                type="password"
-                className={errInfo.type === 'password' ? css.err : ''}
-                value={formData.password}
-                onChange={(e) => {
-                  onChangeFormData(e, 'password')
-                }}
-                placeholder={'登录密码'}
-              />
-              <button
-                className={`${css.submit} ${
-                  visibleSubmit && !actionLoading && css.visible
-                }`}
-                disabled={!visibleSubmit || actionLoading}
-                onClick={onSubmit}
-              >
-                {action.buttonText}
-              </button>
-              {errInfo.message ? (
-                <div className={css.errMsg}>{errInfo.message}</div>
-              ) : null}
-              <div className={css.toolbar}>
-                <div
-                  className={css.toggleButton}
+                <input
+                  type="text"
+                  className={errInfo.type === 'email' ? css.err : ''}
+                  value={formData.email}
+                  onChange={(e) => {
+                    onChangeFormData(e, 'email')
+                  }}
+                  placeholder={'邮箱地址'}
+                />
+                <input
+                  type="password"
+                  className={errInfo.type === 'password' ? css.err : ''}
+                  value={formData.password}
+                  onChange={(e) => {
+                    onChangeFormData(e, 'password')
+                  }}
+                  placeholder={'登录密码'}
+                />
+                <button
+                  className={`${css.submit} ${
+                    visibleSubmit && !actionLoading && css.visible
+                  }`}
+                  disabled={!visibleSubmit || actionLoading}
+                  onClick={onSubmit}
                 >
+                  {action.buttonText}
+                </button>
+                {errInfo.message ? (
+                  <div className={css.errMsg}>{errInfo.message}</div>
+                ) : null}
+                <div className={css.toolbar}>
+                  <div
+                    className={css.toggleButton}
+                  >
+                  </div>
+                  <div className={css.toggleButton} onClick={onToggleAction}>
+                    {/* {action.toggleText} */}
+                    {
+                      action.type === 'signin' && <MutiText text={'没有账号？'} subText={'去创建'} onClick={onToggleAction} />
+                    }
+                    {
+                      action.type === 'signup' && <MutiText text={'已有账号？'} subText={'去登录'} onClick={onToggleAction} />
+                    }
+                  </div>
                 </div>
-                <div className={css.toggleButton} onClick={onToggleAction}>
-                  {/* {action.toggleText} */}
-                  {
-                    action.type === 'signin' && <MutiText text={'没有账号？'} subText={'去创建'} onClick={onToggleAction} />
-                  }
-                  {
-                    action.type === 'signup' && <MutiText text={'已有账号？'} subText={'去登录'} onClick={onToggleAction} />
-                  }
-                </div>
-              </div>
-            </form>
-          )}
+              </form>
+            )}
+          </div>
         </div>
-      </div>
-
-      {/* foot */}
-      <div className={css.foot}>
-        <div className={css.links}>
-          <a
-            className={css.github}
-            href="https://github.com/mybricks/designer-spa-demo"
-            target="_blank"
-          >
-            {Github}Demo源码
-          </a>
-          <a
-            className={css.vscode}
-            href="https://marketplace.visualstudio.com/items?itemName=Mybricks.Mybricks&ssr=false#overview"
-            target="_blank"
-          >
-            {VScode}组件开发
-          </a>
-          <a
-            className={css.docs}
-            href="https://docs.mybricks.world/"
-            target="_blank"
-          >
-            文档教程
-          </a>
-          <a
-            className={css.docs}
-            href="https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg5OTg1OTYwOA==&action=getalbum&album_id=2591211948751650816"
-            target="_blank"
-          >
-            《企业级低代码》
-          </a>
-          <a
-            className={css.copyright}
-            href="https://github.com/mybricks"
-            target="_blank"
-          >
-            @2020 板砖团队
-          </a>
+        <div className={css.foot}>
+          <div className={css.links}>
+            <a
+              className={css.github}
+              href="https://github.com/mybricks/designer-spa-demo"
+              target="_blank"
+            >
+              {Github}Demo源码
+            </a>
+            <a
+              className={css.vscode}
+              href="https://marketplace.visualstudio.com/items?itemName=Mybricks.Mybricks&ssr=false#overview"
+              target="_blank"
+            >
+              {VScode}组件开发
+            </a>
+            <a
+              className={css.docs}
+              href="https://docs.mybricks.world/"
+              target="_blank"
+            >
+              文档教程
+            </a>
+            <a
+              className={css.docs}
+              href="https://mp.weixin.qq.com/mp/appmsgalbum?__biz=Mzg5OTg1OTYwOA==&action=getalbum&album_id=2591211948751650816"
+              target="_blank"
+            >
+              《企业级低代码》
+            </a>
+            <a
+              className={css.copyright}
+              href="https://github.com/mybricks"
+              target="_blank"
+            >
+              @2020 板砖团队
+            </a>
+          </div>
         </div>
       </div>
     </div>
