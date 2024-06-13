dda496a1144585ba164627eff30481e28e4e0091
liubiao03 <cocolbell@163.com>
2024-06-13 15:49:46 +0800

    refactor: console text modify

--- scripts/utils/validate-env.js
+++ scripts/utils/validate-env.js
@@ -8,7 +8,5 @@ function validateDepEnv () {
 }
 
 module.exports = async function validateEnv ({ console }) {
-  console.log(`开始检测数据库`)
   await startInitDatabase({ console })
-  console.log('数据库检测完成')
 }
