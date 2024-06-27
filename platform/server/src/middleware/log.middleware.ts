8b70935bd4dd0c3931d973d68318954e5faae00c
liubiao03 <cocolbell@163.com>
2024-06-27 20:36:16 +0800

    refactor: logger add responsetime

--- platform/server/src/middleware/log.middleware.ts
+++ platform/server/src/middleware/log.middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Logger } from '@mybricks/rocker-commons';
 import { formatBodyOrParamsData } from "../utils/traverse";
 import { ChildLogger } from '../utils/child-logger'
@@ -20,39 +20,51 @@ export function runtimeLogger(option: Option = {}) {
 
   const openMonitor = configuration?.platformConfig?.openMonitor;
 
-  return async (req: Request, res, next: NextFunction) => {
-    let formattedParams = req.query;
-    try {
-      formattedParams = formatBodyOrParamsData(req.query);
-    } catch (err) {
-      formattedParams = req.query;
-    }
-    const application = appNamespaceList.find(namespace => req.path?.startsWith?.(`/${namespace}`)) || 'platform';
-
-    if (req.method !== 'GET' && req.method !== 'DELETE') {
-      formattedParams = req.body;
-      try {
-        formattedParams = formatBodyOrParamsData(req.body);
-      } catch (err) {
-        formattedParams = req.body;
-      }
-    }
-
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const startTime = Date.now()
+    let duration;
+    res.on('finish', () => {
+      duration = Date.now() - startTime
+    })
     res.on('close', () => {
+      if (!duration) {
+        duration = Date.now() - startTime
+      }
       if (!['/paas/api/file/getCooperationUsers'].includes(req.path)) {
-        Logger.info(`[application: ${application}] [code: ${res.statusCode}] [method: ${req.method}] [refer: ${req.headers.referer}] [userAgent: ${req.headers['user-agent']}] [params: ${JSON.stringify(formattedParams || null)}] [path: ${req.path}] [ip: ${req.ip}]`);
+        const application = appNamespaceList.find(namespace => req.originalUrl?.startsWith?.(`/${namespace}`)) || 'platform';
+        Logger.info(`[application: ${application}] [code: ${res.statusCode}] [method: ${req.method}] [duration: ${duration}] [path: ${req.path}] [url: ${req.originalUrl}] [ip: ${req.ip}] [refer: ${req.headers.referer}] [userAgent: ${req.headers['user-agent']}]`);
       } else if (openMonitor) {
         try {
           childLogger.info({
-            ...(formattedParams ?? {}),
+            ...(getParamsFromReq(req) ?? {}),
             refer: req.headers.referer,
-            userAgent:req.headers['user-agent'],
+            userAgent: req.headers['user-agent'],
           })
         } catch (error) {
-          
+
         }
       }
     });
     next();
   };
+}
+
+
+function getParamsFromReq(req: Request) {
+  let formattedParams = req.query;
+  try {
+    formattedParams = formatBodyOrParamsData(req.query);
+  } catch (err) {
+    formattedParams = req.query;
+  }
+  if (req.method !== 'GET' && req.method !== 'DELETE') {
+    formattedParams = req.body;
+    try {
+      formattedParams = formatBodyOrParamsData(req.body);
+    } catch (err) {
+      formattedParams = req.body;
+    }
+  }
+
+  return formattedParams ?? {}
 }
