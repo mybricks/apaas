8e2d29f4ea35d4114c9d803340d4becc31d53e82
liubiao03 <cocolbell@163.com>
2024-07-04 16:05:35 +0800

    ci: install script add pm2 autoinstall

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -4,6 +4,8 @@ const childProcess = require('child_process')
 
 const { loadApps } = require('./shared')
 
+const { pm2CheckAndAutoInstall } = require('./utils/pm2-check');
+
 const Log = require('./utils/log')
 
 const envLog = Log('MyBricks: 启动脚本')
@@ -21,18 +23,29 @@ function installDepsInDir (dir) {
   })
 }
 
-// 平台前端安装依赖
-installDepsInDir(PLATFORM_FE_PATH);
+;(async () => {
+  // 平台前端安装依赖
+  installDepsInDir(PLATFORM_FE_PATH);
 
-// 平台服务端安装依赖
-installDepsInDir(PLATFORM_SERVER_PATH);
+  // 平台服务端安装依赖
+  installDepsInDir(PLATFORM_SERVER_PATH);
 
-// 扫描是否有本地APP
-const apps = loadApps();
-// 本地APP安装依赖
-for (let index = 0; index < apps.length; index++) {
-  const app = apps[index];
-  if (app.hasFe) { // 目前只有前端源码项目需要安装
-    installDepsInDir(app.directory);
+  // 扫描是否有本地APP
+  const apps = loadApps();
+  // 本地APP安装依赖
+  for (let index = 0; index < apps.length; index++) {
+    const app = apps[index];
+    if (app.hasFe) { // 目前只有前端源码项目需要安装
+      installDepsInDir(app.directory);
+    }
   }
-}
+
+  // 检查并自动安装pm2
+  await pm2CheckAndAutoInstall({ console: envLog })
+
+  envLog.log('🎉 所有依赖已安装完成')
+})().catch(err => {
+  envLog.error(err?.message ?? '未知错误')
+  envLog.error('操作已终止')
+  process.exit()
+});
--- scripts/utils/pm2-check.js
+++ scripts/utils/pm2-check.js
@@ -34,8 +34,8 @@ module.exports.pm2CheckAndAutoInstall = async function ({ console }) {
 
   if (!hasPm2) {
     try {
-      console.log('当前全局环境无 PM2，执行 npm i -g pm2 自动安装中...')
-      execSync('npm i -g pm2', { stdio: 'inherit', encoding: 'utf-8' });
+      console.log('当前全局环境无 PM2，执行 npm i -g pm2 --registry=https://registry.npmmirror.com 自动安装中...')
+      execSync('npm i -g pm2 --registry=https://registry.npmmirror.com', { stdio: 'inherit', encoding: 'utf-8' });
     } catch (error) {
       throw new Error('自动安装 PM2 失败，请手动安装 PM2')
     }
