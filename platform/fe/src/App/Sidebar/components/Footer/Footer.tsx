73713a78db6fa4802f3a463290f7b3823104cf54
liubiao03 <cocolbell@163.com>
2024-08-07 11:23:39 +0800

    feat: support user setting close

--- platform/fe/src/App/Sidebar/components/Footer/Footer.tsx
+++ platform/fe/src/App/Sidebar/components/Footer/Footer.tsx
@@ -1,34 +1,41 @@
-import React from "react";
+import React, { useMemo } from 'react'
 
-import { removeCookie } from "@/utils/local";
-import { MenuButton, Modal, Popover } from "@/components";
-import { Signout } from "@/components/icon";
-import { useUserContext, useModalConetxt } from "@/context";
-import { button as AppStoreMenuButton } from "@/Pages/AppStore";
-import { button as AccountMenuButton } from "@/Pages/Account";
-import { button as WebsiteMenuButton } from '@/Pages/Website';
+import { removeCookie } from '@/utils/local'
+import { MenuButton, Modal, Popover } from '@/components'
+import { Signout } from '@/components/icon'
+import { useUserContext, useModalConetxt, useWorkspaceConetxt } from '@/context'
+import { button as AppStoreMenuButton } from '@/Pages/AppStore'
+import { button as AccountMenuButton } from '@/Pages/Account'
+import { button as WebsiteMenuButton } from '@/Pages/Website'
 // import { button as OperationLogMenuButton } from "@/Pages/OperationLog";
-import { button as UserManagementMenuButton } from "@/Pages/UserManagement";
+import { button as UserManagementMenuButton } from '@/Pages/UserManagement'
 // import { button as StaticFilesMenuButton } from "@/Pages/StaticFiles";
-import { button as SettingsMenuButton } from "@/Pages/Settings";
-import { button as ToolsMenuButton  } from '@/Pages/Tools'
+import { button as SettingsMenuButton } from '@/Pages/Settings'
+import { button as ToolsMenuButton } from '@/Pages/Tools'
 
-import css from "./Footer.less";
+import css from './Footer.less'
 
 const Footer = () => {
-  const { user: { name, email, avatar, isAdmin } } = useUserContext();
-  const { showModal } = useModalConetxt();
+  const {
+    user: { name, email, avatar, isAdmin },
+  } = useUserContext()
+  const { system } = useWorkspaceConetxt()
+  const { showModal } = useModalConetxt()
 
   const handleSignout = () => {
     showModal(Modal.Confirmation, {
-      title: "确认退出登录吗？",
-      content: "退出后将跳转登录页",
+      title: '确认退出登录吗？',
+      content: '退出后将跳转登录页',
       onOk() {
         removeCookie('mybricks-login-user')
         location.href = '/'
-      }
+      },
     })
   }
+
+  const noMenuItems = useMemo(() => {
+    return !system.openUserInfoSetting && !system.openLogout
+  }, [system.openUserInfoSetting, system.openLogout])
 
   return (
     <div className={css.footer}>
@@ -46,28 +53,27 @@ const Footer = () => {
       <div className={css.personal}>
         <Popover
           placement="topLeft"
-          title={(
+          title={
             <>
               {name && <div className={css.name}>{name}</div>}
               <div className={css.email}>{email}</div>
             </>
-          )}
-          content={(
+          }
+          content={
             <>
-              <AccountMenuButton />
-              <MenuButton
-                icon={<Signout />}
-                onClick={handleSignout}
-              >
-                退出登录
-              </MenuButton>
+              {system.openUserInfoSetting && <AccountMenuButton />}
+              {system.openLogout && (
+                <MenuButton icon={<Signout />} onClick={handleSignout}>
+                  退出登录
+                </MenuButton>
+              )}
             </>
-          )}
+          }
           arrow={false}
-          overlayClassName={css.popover}
+          overlayClassName={`${css.popover} ${noMenuItems ? '' : css.notNone}`}
         >
           <button>
-            <img src={avatar}/>
+            <img src={avatar} />
             <span>{name || email}</span>
           </button>
         </Popover>
@@ -76,4 +82,4 @@ const Footer = () => {
   )
 }
 
-export default Footer;
+export default Footer
